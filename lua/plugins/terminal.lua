8fabbd0ce921c1339ff3ab7de88f98fcda774549
HeJiaJunPan <30548025+HeJiaJunPan@users.noreply.github.com>
2024-03-30 23:33:12 +0800

    浮动终端

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,12 @@
+return {
+    'akinsho/toggleterm.nvim',
+    event = 'VeryLazy',
+    opts = {
+        open_mapping = '<c-t>',
+        hide_numbers = true,
+        autochdir = false,
+        direction = 'float',
+        close_on_exit = true,
+        auto_scroll = true
+    }
+}
