4c600d4933e3bc908cadf1af1bfe3b6e24cef6a7
HeJiaJunPan <30548025+HeJiaJunPan@users.noreply.github.com>
2024-03-30 22:58:29 +0800

    升级切换主题配置

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,25 +1,48 @@
-return {
-    {
-        "folke/tokyonight.nvim",
-        -- "navarasu/onedark.nvim",
-        dependencies = {
-            "nvim-lualine/lualine.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "utilyre/barbecue.nvim",
-            "SmiteshP/nvim-navic",
-        },
-        config = function()
-            local themeName = "tokyonight"
-            vim.cmd[[colorscheme tokyonight-storm]]
-            require('lualine').setup({
-                options = {
-                    theme = themeName
-                },
-            })
-
-            require('barbecue').setup {
-                theme = themeName,
-            }
-        end
+local themeName = 'onedark'
+local themeUrls = {
+    tokyonight = 'folke/tokyonight.nvim',
+    onedark = 'navarasu/onedark.nvim'
+}
+local themeConfigs = {
+    onedark = {
+        style = 'dark',
+        term_colors = true,
+        transparent = true
     },
+    tokyonight = {
+        style = 'storm',
+        terminal_colors = true,
+        transparent = true,
+        styles = {
+            sidebars = 'transparent',
+            floats = 'transparent'
+        }
+    }
+}
+
+return {
+    themeUrls[themeName],
+    dependencies = {
+        'nvim-lualine/lualine.nvim',
+        'nvim-tree/nvim-web-devicons',
+        'utilyre/barbecue.nvim',
+        'SmiteshP/nvim-navic',
+    },
+    config = function()
+        require(themeName).setup(
+            themeConfigs[themeName]
+        )
+
+        vim.cmd('colorscheme ' .. themeName)
+
+        require('lualine').setup({
+            options = {
+                theme = themeName
+            },
+        })
+
+        require('barbecue').setup {
+            theme = themeName,
+        }
+    end
 }
