8484c6a829bc19cfd6a49c3109dc49eaa4cacc34
HeJiaJunPan <30548025+HeJiaJunPan@users.noreply.github.com>
2024-04-04 17:12:51 +0800

    兼容内置OSC52剪贴板功能

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,18 +1,6 @@
--- 自定义provider-clipboard
-local function copy(lines, _)
-    local osc52 = require('osc52')
-    osc52.setup({
-        trim = true
-    })
-    osc52.copy(table.concat(lines, '\n'))
-end
-
-local function paste()
-    return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
-end
-
--- 如果当前版本不支持内置OSC52，则使用插件添加OSC52功能
+-- 配置剪贴板选项
 vim.opt.clipboard = 'unnamedplus'
+-- neovim内置OSC52检测
 local ok, clipboard = pcall(require, 'vim.ui.clipboard.osc52')
 if ok then
     vim.g.clipboard = {
@@ -22,16 +10,33 @@ if ok then
     }
 
     return {}
-else
-    vim.g.clipboard = {
-        name = 'osc52',
-        copy = {['+'] = copy, ['*'] = copy},
-        paste = {['+'] = paste, ['*'] = paste},
-    }
 
+-- 当前版本不支持内置OSC52，添加第三方OSC52插件
+else
     return {
         'ojroques/nvim-osc52',
-        event = 'VeryLazy',
-        config = true
+        -- 新建文件或读取已存在的文件时加载插件
+        -- 从面板新建缓冲区时不会加载此插件
+        event = {'BufReadPost', 'BufNewFile'},
+        config = function()
+            -- 自定义provider-clipboard
+            local copy = function(osc52)
+                return function(lines, _)
+                    osc52.copy(table.concat(lines, '\n'))
+                end
+            end
+
+            local paste = function()
+                return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+            end
+
+            local osc52 = require('osc52')
+            osc52.setup({trim = true})
+            vim.g.clipboard = {
+                name = 'osc52',
+                copy = {['+'] = copy(osc52), ['*'] = copy(osc52)},
+                paste = {['+'] = paste, ['*'] = paste},
+            }
+        end
     }
 end
