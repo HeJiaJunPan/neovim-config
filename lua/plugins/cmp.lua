ddebd57c05aa2448b73cf88d240d0532c08f7f92
HeJiaJunPan <30548025+HeJiaJunPan@users.noreply.github.com>
2024-04-05 00:02:52 +0800

    Update cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,9 +9,13 @@ return {
         'hrsh7th/cmp-cmdline',
         -- 代码片段，LuaSnip补全依赖cmp_luasnip
         -- friendly-snippets是vscode-like格式的第三方代码片段库，为LuaSnip提供代码片段
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        'rafamadriz/friendly-snippets',
+        {
+            'saadparwaiz1/cmp_luasnip',
+            dependencies = {
+                'L3MON4D3/LuaSnip',
+                'rafamadriz/friendly-snippets',
+            }
+        },
         -- 类vscode外观的补全界面
         'onsails/lspkind.nvim'
     },
