5f2d8b86882309372057d55ec6c0daa876225850
HeJiaJunPan <30548025+HeJiaJunPan@users.noreply.github.com>
2024-04-10 23:43:47 +0800

    Update terminal.lua

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,8 +1,29 @@
+function run()
+    local fileExtension = vim.fn.expand('%:e')
+    local cmd = nil
+    if fileExtension == 'py' then
+        cmd = 'python3 %'
+    elseif fileExtension == 'lua' then
+        cmd = 'lua %'
+    elseif fileExtension == 'js' then
+        cmd = 'node %'
+    elseif fileExtension == 'sh' then
+        cmd = 'sh %'
+    end
+
+    if cmd  then
+        return string.format([[<cmd>TermExec cmd="%s"<cr>]], cmd)
+    else
+        return [[<cmd>echo "No executor defined"<cr>]]
+    end
+end
+
 return {
     'akinsho/toggleterm.nvim',
     -- event = 'VeryLazy',
     keys = {
-        {'<c-t>', '<cmd>ToggleTerm<cr>'}
+        {'<c-t>', '<cmd>ToggleTerm<cr>'},
+        {'<F5>', run()}
     },
     opts = {
         open_mapping = '<c-t>',
