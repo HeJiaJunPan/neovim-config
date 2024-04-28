13641b25bd8c53d49e0ef26aa2d9310eb0523c42
HeJiaJunPan <30548025+HeJiaJunPan@users.noreply.github.com>
2024-04-28 23:01:38 +0800

    Update utils.lua

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -82,6 +82,27 @@ return {
         config = true,
     },
     {
+        -- 为每个窗口用字符标记，快速进行跳转
+        "s1n7ax/nvim-window-picker",
+        opts = {
+            filter_rules = {
+                include_current_win = true,
+                filetype = { "fidget", "neo-tree" }
+            }
+        },
+        keys = {
+            {
+                "<c-w>",
+                function()
+                    local window_number = require('window-picker').pick_window()
+                    if window_number then
+                        vim.api.nvim_set_current_win(window_number)
+                    end
+                end
+            }
+        }
+    },
+    {
         -- gcc:行注释
         -- gbc:块注释
         "numToStr/Comment.nvim",
